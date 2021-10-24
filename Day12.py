b1e268bfcd543ec00de1598bc9f9bcd0fc5dbf22
Sarvesh Bagwe <76241087+Sarvesh1814@users.noreply.github.com>
2021-10-24 22:33:57 +0530

    Create Day12.py

---
+++ Day12.py
@@ -0,0 +1,12 @@
+ if a_s < 40:
+            return "T"
+        elif a_s < 55:
+            return "D"
+        elif a_s < 70:
+            return "P"
+        elif a_s < 80:
+            return "A"
+        elif a_s < 90:
+            return "E"
+        else:
+            return "O"
